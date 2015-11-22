dc4b6ce802bf9295a49ac0f5ed12b413e9e4f066
jessicaribeiro <jessicafaaribeiro@gmail.com>
2015-11-22 17:18:55 +0000

    initial layout commit

---
+++ js/custom.js
@@ -0,0 +1,388 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/** ******  left menu  *********************** **/
+$(function () {
+    $('#sidebar-menu li ul').slideUp();
+    $('#sidebar-menu li').removeClass('active');
+
+    $('#sidebar-menu li').click(function () {
+        if ($(this).is('.active')) {
+            $(this).removeClass('active');
+            $('ul', this).slideUp();
+            $(this).removeClass('nv');
+            $(this).addClass('vn');
+        } else {
+            $('#sidebar-menu li ul').slideUp();
+            $(this).removeClass('vn');
+            $(this).addClass('nv');
+            $('ul', this).slideDown();
+            $('#sidebar-menu li').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
+    $('#menu_toggle').click(function () {
+        if ($('body').hasClass('nav-md')) {
+            $('body').removeClass('nav-md');
+            $('body').addClass('nav-sm');
+            $('.left_col').removeClass('scroll-view');
+            $('.left_col').removeAttr('style');
+            $('.sidebar-footer').hide();
+
+            if ($('#sidebar-menu li').hasClass('active')) {
+                $('#sidebar-menu li.active').addClass('active-sm');
+                $('#sidebar-menu li.active').removeClass('active');
+            }
+        } else {
+            $('body').removeClass('nav-sm');
+            $('body').addClass('nav-md');
+            $('.sidebar-footer').show();
+
+            if ($('#sidebar-menu li').hasClass('active-sm')) {
+                $('#sidebar-menu li.active-sm').addClass('active');
+                $('#sidebar-menu li.active-sm').removeClass('active-sm');
+            }
+        }
+    });
+});
+
+// Player Photo
+imageArray = new Array("","images/players/nadal.png", "images/players/Federer.png", "images/players/Djokovic.png"); 
+flagArray = new Array("","images/flags/es.png", "images/flags/ch.png", "images/flags/rs.png"); 
+
+
+altArray = new Array("", "Rafael Nadal", "Roger Federer", "Novak Djokovic"); 
+
+// Player List 
+function changePlayer1() {
+    var playerList1 = document.getElementById("playerList1");
+    var index1 = playerList1.selectedIndex;
+    var strUser1 = playerList1.options[index1].value;
+
+    document.getElementById("list1").innerHTML = strUser1;
+    document.getElementById("playerList1").selectedIndex = 0;
+
+    document.getElementById("player_photo1").src = imageArray[index1]; 
+    document.getElementById("player_photo1").alt = altArray[index1];
+
+    document.getElementById("flag_photo1").src = flagArray[index1]; 
+
+
+    //document.getElementById("list").value = playerList.options[playerList.selectedIndex].text;
+}
+
+function changePlayer2() {
+    var playerList2 = document.getElementById("playerList2");
+    var index2 = playerList2.selectedIndex;
+    var strUser2 = playerList2.options[index2].value;
+
+    document.getElementById("list2").innerHTML = strUser2;
+    document.getElementById("playerList2").selectedIndex = 0;
+
+    document.getElementById("player_photo2").src = imageArray[index2]; 
+    document.getElementById("player_photo2").alt = altArray[index2];
+
+    document.getElementById("flag_photo2").src = flagArray[index2]; 
+
+
+    //document.getElementById("list").value = playerList.options[playerList.selectedIndex].text;
+}
+
+function changePlayer3() {
+    var playerList3 = document.getElementById("playerList3");
+    var index3 = playerList3.selectedIndex;
+    var strUser3 = playerList3.options[index3].value;
+
+    document.getElementById("list3").innerHTML = strUser3;
+    document.getElementById("playerList3").selectedIndex = 0;
+
+    document.getElementById("player_photo3").src = imageArray[index3]; 
+    document.getElementById("player_photo3").alt = altArray[index3];
+
+    document.getElementById("flag_photo3").src = flagArray[index3]; 
+
+
+    //document.getElementById("list").value = playerList.options[playerList.selectedIndex].text;
+}
+
+
+
+
+
+
+/* Sidebar Menu active class */
+$(function () {
+    var url = window.location;
+    $('#sidebar-menu a[href="' + url + '"]').parent('li').addClass('current-page');
+    $('#sidebar-menu a').filter(function () {
+        return this.href == url;
+    }).parent('li').addClass('current-page').parent('ul').slideDown().parent().addClass('active');
+});
+
+/** ******  /left menu  *********************** **/
+
+
+
+/** ******  tooltip  *********************** **/
+$(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+    /** ******  /tooltip  *********************** **/
+    /** ******  progressbar  *********************** **/
+if ($(".progress .progress-bar")[0]) {
+    $('.progress .progress-bar').progressbar(); // bootstrap 3
+}
+/** ******  /progressbar  *********************** **/
+/** ******  switchery  *********************** **/
+if ($(".js-switch")[0]) {
+    var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+    elems.forEach(function (html) {
+        var switchery = new Switchery(html, {
+            color: '#26B99A'
+        });
+    });
+}
+/** ******  /switcher  *********************** **/
+/** ******  collapse panel  *********************** **/
+// Close ibox function
+$('.close-link').click(function () {
+    var content = $(this).closest('div.x_panel');
+    content.remove();
+});
+
+
+
+//    vertical-align: -6px;
+
+
+
+
+// Collapse ibox function
+$('.collapse-link').click(function () {
+    var x_panel = $(this).closest('div.x_panel');
+    var button = $(this).find('i');
+    var content = x_panel.find('div.x_content');
+    content.slideToggle(200);
+    (x_panel.hasClass('fixed_height_390') ? x_panel.toggleClass('').toggleClass('fixed_height_390') : '');
+    (x_panel.hasClass('fixed_height_320') ? x_panel.toggleClass('').toggleClass('fixed_height_320') : '');
+    button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+    setTimeout(function () {
+        x_panel.resize();
+    }, 50);
+});
+/** ******  /collapse panel  *********************** **/
+/** ******  iswitch  *********************** **/
+if ($("input.flat")[0]) {
+    $(document).ready(function () {
+        $('input.flat').iCheck({
+            checkboxClass: 'icheckbox_flat-green',
+            radioClass: 'iradio_flat-green'
+        });
+    });
+}
+/** ******  /iswitch  *********************** **/
+/** ******  star rating  *********************** **/
+// Starrr plugin (https://github.com/dobtco/starrr)
+var __slice = [].slice;
+
+(function ($, window) {
+    var Starrr;
+
+    Starrr = (function () {
+        Starrr.prototype.defaults = {
+            rating: void 0,
+            numStars: 5,
+            change: function (e, value) {}
+        };
+
+        function Starrr($el, options) {
+            var i, _, _ref,
+                _this = this;
+
+            this.options = $.extend({}, this.defaults, options);
+            this.$el = $el;
+            _ref = this.defaults;
+            for (i in _ref) {
+                _ = _ref[i];
+                if (this.$el.data(i) != null) {
+                    this.options[i] = this.$el.data(i);
+                }
+            }
+            this.createStars();
+            this.syncRating();
+            this.$el.on('mouseover.starrr', 'span', function (e) {
+                return _this.syncRating(_this.$el.find('span').index(e.currentTarget) + 1);
+            });
+            this.$el.on('mouseout.starrr', function () {
+                return _this.syncRating();
+            });
+            this.$el.on('click.starrr', 'span', function (e) {
+                return _this.setRating(_this.$el.find('span').index(e.currentTarget) + 1);
+            });
+            this.$el.on('starrr:change', this.options.change);
+        }
+
+        Starrr.prototype.createStars = function () {
+            var _i, _ref, _results;
+
+            _results = [];
+            for (_i = 1, _ref = this.options.numStars; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
+                _results.push(this.$el.append("<span class='glyphicon .glyphicon-star-empty'></span>"));
+            }
+            return _results;
+        };
+
+        Starrr.prototype.setRating = function (rating) {
+            if (this.options.rating === rating) {
+                rating = void 0;
+            }
+            this.options.rating = rating;
+            this.syncRating();
+            return this.$el.trigger('starrr:change', rating);
+        };
+
+        Starrr.prototype.syncRating = function (rating) {
+            var i, _i, _j, _ref;
+
+            rating || (rating = this.options.rating);
+            if (rating) {
+                for (i = _i = 0, _ref = rating - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+                    this.$el.find('span').eq(i).removeClass('glyphicon-star-empty').addClass('glyphicon-star');
+                }
+            }
+            if (rating && rating < 5) {
+                for (i = _j = rating; rating <= 4 ? _j <= 4 : _j >= 4; i = rating <= 4 ? ++_j : --_j) {
+                    this.$el.find('span').eq(i).removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+                }
+            }
+            if (!rating) {
+                return this.$el.find('span').removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+            }
+        };
+
+        return Starrr;
+
+    })();
+    return $.fn.extend({
+        starrr: function () {
+            var args, option;
+
+            option = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+            return this.each(function () {
+                var data;
+
+                data = $(this).data('star-rating');
+                if (!data) {
+                    $(this).data('star-rating', (data = new Starrr($(this), option)));
+                }
+                if (typeof option === 'string') {
+                    return data[option].apply(data, args);
+                }
+            });
+        }
+    });
+})(window.jQuery, window);
+
+$(function () {
+    return $(".starrr").starrr();
+});
+
+$(document).ready(function () {
+
+    $('#stars').on('starrr:change', function (e, value) {
+        $('#count').html(value);
+    });
+
+
+    $('#stars-existing').on('starrr:change', function (e, value) {
+        $('#count-existing').html(value);
+    });
+
+});
+/** ******  /star rating  *********************** **/
+/** ******  table  *********************** **/
+$('table input').on('ifChecked', function () {
+    check_state = '';
+    $(this).parent().parent().parent().addClass('selected');
+    countChecked();
+});
+$('table input').on('ifUnchecked', function () {
+    check_state = '';
+    $(this).parent().parent().parent().removeClass('selected');
+    countChecked();
+});
+
+var check_state = '';
+$('.bulk_action input').on('ifChecked', function () {
+    check_state = '';
+    $(this).parent().parent().parent().addClass('selected');
+    countChecked();
+});
+$('.bulk_action input').on('ifUnchecked', function () {
+    check_state = '';
+    $(this).parent().parent().parent().removeClass('selected');
+    countChecked();
+});
+$('.bulk_action input#check-all').on('ifChecked', function () {
+    check_state = 'check_all';
+    countChecked();
+});
+$('.bulk_action input#check-all').on('ifUnchecked', function () {
+    check_state = 'uncheck_all';
+    countChecked();
+});
+
+function countChecked() {
+        if (check_state == 'check_all') {
+            $(".bulk_action input[name='table_records']").iCheck('check');
+        }
+        if (check_state == 'uncheck_all') {
+            $(".bulk_action input[name='table_records']").iCheck('uncheck');
+        }
+        var n = $(".bulk_action input[name='table_records']:checked").length;
+        if (n > 0) {
+            $('.column-title').hide();
+            $('.bulk-actions').show();
+            $('.action-cnt').html(n + ' Records Selected');
+        } else {
+            $('.column-title').show();
+            $('.bulk-actions').hide();
+        }
+    }
+    /** ******  /table  *********************** **/
+    /** ******    *********************** **/
+    /** ******    *********************** **/
+    /** ******    *********************** **/
+    /** ******    *********************** **/
+    /** ******    *********************** **/
+    /** ******    *********************** **/
+    /** ******  Accordion  *********************** **/
+
+$(function () {
+    $(".expand").on("click", function () {
+        $(this).next().slideToggle(200);
+        $expand = $(this).find(">:first-child");
+
+        if ($expand.text() == "+") {
+            $expand.text("-");
+        } else {
+            $expand.text("+");
+        }
+    });
+});
+
+/** ******  Accordion  *********************** **/
+/** ******  scrollview  *********************** **/
+$(document).ready(function () {
+  
+            $(".scroll-view").niceScroll({
+                touchbehavior: true,
+                cursorcolor: "rgba(42, 63, 84, 0.35)"
+            });
+
+});
+/** ******  /scrollview  *********************** **/
